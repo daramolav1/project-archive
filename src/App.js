27240fde245754239c616ba2efbb867818d1f74d
Victor <victorddaramola@gmail.com>
2022-09-01 05:45:04 -0400

    Imported various project components and NoMatch for undefined route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+import TermsOfService from "./pages/terms-of-service";
+import NoMatch from "./pages/404";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header />
+              <Portfolio />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/terms-of-service"
+          element={
+            <>
+              <TermsOfService />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route path="*" element={<NoMatch />}></Route>
+      </Routes>
     </div>
   );
 }
