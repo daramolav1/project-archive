9cc5b127068077eb653b0987a41523efd8fcda42
Victor <victorddaramola@gmail.com>
2022-09-01 05:48:39 -0400

    Imported all portfolio components

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "./Hero";
+import About from "./About";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+function Portfolio() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
+
+export default Portfolio;
