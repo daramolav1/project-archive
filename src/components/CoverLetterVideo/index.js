418be0f0006d24b78a156504cca774426869d1c5
Victor <victorddaramola@gmail.com>
2022-09-01 14:26:14 -0400

    Video Cover Letter component

--- src/components/CoverLetterVideo/index.js
+++ src/components/CoverLetterVideo/index.js
@@ -3,18 +3,21 @@ import "./styles.css";
 
 function CoverLetterVideo() {
   return (
-    <div>
+    <>
       <div className="wistia_responsive_padding">
         <div className="wistia_responsive_wrapper">
           <iframe
-            src="https://fast.wistia.net/embed/iframe/mfmysrj9hz?videoFoam=true"
-            title=" [Example Video] Wistia Video Essentials"
+            src="https://fast.wistia.net/embed/iframe/qdru1w8d7j?videoFoam=true"
+            title="Video Cover Letter on 9-1-22 at 1.44 PM"
             allow="autoplay; fullscreen"
             allowtransparency="true"
             frameBorder="0"
             scrolling="no"
             className="wistia_embed"
             name="wistia_embed"
+            msallowfullscreen
+            width="100%"
+            height="100%"
           ></iframe>
         </div>
       </div>
@@ -22,7 +25,7 @@ function CoverLetterVideo() {
         src="https://fast.wistia.net/assets/external/E-v1.js"
         async
       ></script>
-    </div>
+    </>
   );
 }
 
